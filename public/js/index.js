b5f8f480848673ff9883ab15c917b2d104a273ff
Kevin <nguyenk215@yahoo.com>
2018-11-02 10:49:26 -0700

    greet new user and alert others

---
+++ public/js/index.js
@@ -0,0 +1,13 @@
+var socket = io(); // creates our connection, request from client to server to keep open connection.
+
+socket.on('connect', function() {
+  console.log('Connected to server');
+});
+
+socket.on('disconnect', function() {
+  console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function(message) {
+  console.log('New Message', message);
+});
